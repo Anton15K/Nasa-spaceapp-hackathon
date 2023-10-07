4d03f827aecfa3a58d48d49c564b451c26456568
Ivan Osipenko <padavan2007@gmail.com>
2023-10-07 18:51:49 +0300

    Update video_resolve.py

--- video_resolve.py
+++ video_resolve.py
@@ -2,13 +2,61 @@ import cv2
 import numpy as np
 import time
 
+##### define  musical parameters
+scale = MIXOLYDIAN_SCALE
 
+minPitch = 0  # MIDI pitch (0-127)
+maxPitch = 127
+
+minDuration = 0.8  # duration (1.0 is QN)
+maxDuration = 6.0
+
+minVolume = 0  # MIDI velocity (0-127)
+maxVolume = 127
 def create_video(out):
     pass
-
+def process_lines(result, line, frame):
+    sz = len(line)
+    average_r = 0
+    average_g = 0
+    average_b = 0
+    for el in line:
+        x, y = el
+        #print(frame[y, x])
+        R, G, B = frame[y, x]
+        average_r += R
+        average_g += G
+        average_b += B
+    average_r //= sz
+    average_g //= sz
+    average_b //= sz
+    result[0] += average_r
+    result[1] += average_g
+    result[2] += average_b
+    #print(average_r, average_g, average_b)
+    return result
 def convert_to_sound(pixels_to_convert):
-    pass
+    #print(pixels_to_convert)
+    red, green, blue = pixels_to_convert  # get pixel RGB value
 
+    luminosity = (red + green + blue) / 3  # calculate brightness
+
+    # map luminosity to pitch (the brighter the pixel, the higher
+    # the pitch) using specified scale
+    pitch = mapScale(luminosity, 0, 255, minPitch, maxPitch, scale)
+
+    # map red value to duration (the redder the pixel, the longer
+    # the note)
+    #duration = mapValue(red, 0, 255, minDuration, maxDuration)
+
+    # map blue value to dynamic (the bluer the pixel, the louder
+    # the note)
+    #dynamic = mapValue(blue, 0, 255, minVolume, maxVolume)
+
+    # create note and return it to caller
+    #note = Note(pitch, duration, dynamic)
+
+    # done sonifying this pixel, so return result'''
 def bresenham_line(x0, y0, x1, y1):
     dx = abs(x1 - x0)
     dy = abs(y1 - y0)
@@ -69,38 +117,75 @@ def resolve_video(video_path):
 
     prev_x = 0
     prev_y = 0
+
     sz = frame_height + frame_width
     c = sz // frame_count + 1
-    print(frame_count, sz, c)
+    print(frame_count, sz, c, fps)
+
+    average_colour_for_second = [0, 0, 0]
+    number_of_used_lines = 0
+    a = 0
     if 0 == 1:
         pass
         #c = frame_count // sz
     # Loop through each frame in the video array and draw a circle on the center pixel
     else:
-        fourcc = cv2.VideoWriter_fourcc(*'X264')
+        fourcc = cv2.VideoWriter_fourcc(*'MP4V')
         out = cv2.VideoWriter('output.mp4', fourcc, 30.0, (frame_width, frame_height))
         for frame in video_array:
+            number_of_used_lines += 1
+            average_colour_for_frame = [0, 0, 0]
             for i in range(prev_x, min(prev_x + c, frame_width)):
             # Draw a circle on the center pixel of the frame
                 #cv2.circle(frame, center_point, 1, (0, 0, 255), -1)
 
                 # Draw a line from the center point to each corner pixel of the frame
-                cv2.line(frame, (i, frame_height), (frame_width-i, 0), (R_colour, G_colour, B_colour), 1)
-                convert_to_sound(bresenham_line(i, frame_height, frame_width-i, 0))
+                x0 = i
+                y0 = frame_height - 1
+                x1 = frame_width-i-1
+                y1 = 0
+                process_lines(average_colour_for_frame, bresenham_line(x0, y0, x1, y1), frame)
+                cv2.line(frame, (x0, y0), (x1, y1), (R_colour, G_colour, B_colour), 1)
+                '''or el in bresenham_line(x0, y0, x1, y1):
+                    print(el[0], el[1])'''
                 '''cv2.imshow("Frame", frame)
                 cv2.waitKey(1000//30)'''
-
+            average_colour_for_second[0] += average_colour_for_frame[0] // c
+            average_colour_for_second[1] += average_colour_for_frame[1] // c
+            average_colour_for_second[2] += average_colour_for_frame[2] // c
             prev_x += c
             if prev_x < frame_width:
                 '''cv2.imshow("Frame", frame)
                 cv2.waitKey(1000//fps)'''
+                if number_of_used_lines % fps == 0:
+                    average_colour_for_second[0] //= fps
+                    average_colour_for_second[1] //= fps
+                    average_colour_for_second[2] //= fps
+                    print(number_of_used_lines)
+                    convert_to_sound(average_colour_for_second)
+                    average_colour_for_second = [0, 0, 0]
+                    a += 1
                 out.write(frame)
-                
                 continue
             for i in range(prev_y, min(prev_y + c, frame_height)):
-                cv2.line(frame, (frame_width, frame_height-i), (0, 0+i), (R_colour, G_colour, B_colour), 1)
+                x0 = frame_width - 1
+                y0 = frame_height - i - 1
+                x1 = 0
+                y1 = i
+                process_lines(average_colour_for_frame, bresenham_line(x0, y0, x1, y1), frame)
+                cv2.line(frame, (x0, y0), (x1, y1), (R_colour, G_colour, B_colour), 1)
                 #cv2.line(frame, center_point, (center_x-i, 0), (0, 0, 0), 1)
-
+            average_colour_for_second[0] += average_colour_for_frame[0] // c
+            average_colour_for_second[1] += average_colour_for_frame[1] // c
+            average_colour_for_second[2] += average_colour_for_frame[2] // c
+            if number_of_used_lines % fps == 0:
+                average_colour_for_second[0] //= fps
+                average_colour_for_second[1] //= fps
+                average_colour_for_second[2] //= fps
+                print(number_of_used_lines)
+                convert_to_sound(average_colour_for_second)
+                average_colour_for_second = [0, 0, 0]
+                a += 1
             '''cv2.imshow("Frame", frame)
             cv2.waitKey(1000//fps)'''
             out.write(frame)
@@ -116,11 +201,7 @@ def resolve_video(video_path):
             else:
                 prev_x = 0
                 prev_y = 0
+        print(a)
         out.release()
 
-
-
-
-print(resolve_video("images/saturn.mp4"))
-
-
+resolve_video("images/Hydra.mp4")
